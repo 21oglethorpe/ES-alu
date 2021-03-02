69903d9c9c2882a01fdb0da2cc28955377ccd454
21oglethorpe <jc2376@scarletmail.rutgers.edu>
2021-03-02 03:52:18 +0000

    Update my_alu.vhd

--- my_alu.vhd
+++ my_alu.vhd
@@ -33,15 +33,32 @@ use IEEE.numeric_std.all;
 
 entity my_alu is
 port ( A, B, x : in std_logic_vector(3 downto 0);
+        clk : in std_logic;
         led : out std_logic_vector(3 downto 0));
 end my_alu;
 
 architecture Behavioral of my_alu is
-
 begin
-case x is
-when "0000" => led <= std_logic_vector(unsigned(A)+ unsigned(B));
+    process(x)
+    begin 
+    case (x) is
+        when "0000"   => led <= std_logic_vector(unsigned(A)+unsigned(B));
+        when "0001"  => led <= std_logic_vector(unsigned(A)-unsigned(B));
+        when "0011"  => led <= std_logic_vector(unsigned(A)+1);
+        when "0100"  => led <= std_logic_vector(unsigned(A)-1);
+        when "0101"  => led <= std_logic_vector(0-unsigned(A));
+        when "0110"  => if A > B then led <= "1111"; else led <= "0000"; end if;
+        when "0111"  => led <= std_logic_vector(unsigned(A)+unsigned(B));
+        when "1000"  => led <= std_logic_vector(unsigned(A)+unsigned(B));
+        when "1001"  => led <= NOT A;
+        when "1010"  => led <= A and B;
+        when "1011"  => led <= A or B;
+        when "1100"  => led <= A xor B;
+        when "1101"  => led <= A xnor B;
+        when "1110"  => led <= A NAND B;
+        when "1111"  => led <= A nor B;
+        end case;
 
-end case;
+end process;
 
 end Behavioral;
